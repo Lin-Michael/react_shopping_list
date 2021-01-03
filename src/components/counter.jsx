7de430373433630d7dc0f21e2a19fa6768024be5
mike <38706072+MichaelJonathanLin@users.noreply.github.com>
2021-01-02 23:30:24 -0500

    Aded a single source of truth, all data is now controlled in the Counters component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,31 +1,36 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {
-        value: this.props.value,
-        
-    };
-    //increments the state
-    handleIncrement = () =>{
-        console.log("Increment Clicked", this);
-        this.setState({ value: this.state.value + 1});//setState is a React function
-    }
+
   render() {
     return (
       <div className="list-item">
-        <span className={this.getBadgeClasses()}>{this.state.value}</span>
-        <button onClick={this.handleIncrement}
-        className = "btn btn-secondary btn-sm">
-            Increment
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
         </button>
-        <button onClick={this.props.onDelete} className="btn btn-danger btn-sm m-2">Delete</button>
+        <button
+          onClick={() =>this.props.onDelete(this.props.counter.id)}//the onClick handler cannot be a direct reference to a function. To solve this, recreate it as an arrow function
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
       </div>
     );
   }
-  getBadgeClasses(){
-      return (this.state.count === 0) ? "badge badge-warning": "badge badge-primary";
+  getBadgeClasses() {
+    return this.props.counter.value === 0
+      ? "badge badge-warning"
+      : "badge badge-primary";
   }
 
+  formatCount(){
+      const {value} = this.props.counter; // gets this.props.counter.value and assigns it to a local value variable
+      return value === 0 ? "Zero" : value;
+  }
 }
 
 export default Counter;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,21 +4,51 @@ import Counter from "./counter";
 class Counters extends Component {
   state = {
     counters: [
-        { id: 1, value: 4 },
-        { id: 2, value: 0 },
-        { id: 3, value: 0 },
-        { id: 4, value: 0 }
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
     ],
   };
 
-  handleDelete = () =>{
-      console.log('Event Handler Called');
+  handleIncrement = counter => {
+      const counters=[...this.state.counters];
+      const index = counters.indexOf(counter);
+      counters[index] = {...counter};
+      counters[index].value++;
+      this.setState({counters});
+  }
+
+  handleReset = () =>{
+      const counters = this.state.counters.map(c => {
+          c.value =0;
+          return c;
+      });
+      this.setState({counters});
+  };
+
+  handleDelete = (counterId) => {
+    console.log("Event Handler Called", counterId);
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters: counters });
   };
   render() {
     return (
       <div>
-        {this.state.counters.map(counter=> 
-        <Counter key={counter.id} onDelete={this.handleDelete} value={counter.value}/>)}
+          <button
+           onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2">Reset</button>
+        {this.state.counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            counter={counter}
+            //Instead of this, pass the entire counter as a props so that if the Counter objec changes in the future, it is also reflected in this parent
+            //value={counter.value}
+            //id={counter.id}
+          />
+        ))}
       </div>
     );
   }
